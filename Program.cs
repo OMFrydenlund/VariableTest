4a54112f2a12b3190304dd4fe47dc429f6657694
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-15 13:34:00 +0100

    Comment

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             int a = 5;
             decimal b = 3.0M;
-            decimal sum = a + b;
+            decimal sum = a + b; //decimal becomes the priority declaration in this example, higher priority than int due to previously declared decimal
 
             Console.WriteLine($"A + B = {sum}");
         }

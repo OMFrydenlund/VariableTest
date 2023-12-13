5eeaa737a3de77d4535db648ab6a687c0da76fed
OMFrydenlund <migfrydenlund@gmail.com>
2023-12-13 10:29:07 +0100

    Regrets

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int a = 5;
             decimal b = 3.0M;
-            decimal sum = a + b; //decimal becomes the priority declaration in this example, higher priority than int due to previously declared decimal
+            decimal
+                sum = a + b; //decimal becomes the priority declaration in this example, higher priority than int due to previously declared decimal
 
             Console.WriteLine($"A + B = {sum}");
         }
